c9eea00fed22314005d763794462825b76b4001c
Sebas Quesada <s_quesada96@hotmail.com>
2017-03-01 20:12:42 -0600

    Rediseño de letras
    Isa ahí estiré un toque las letras nada más.

--- fontROM.v
+++ fontROM.v
@@ -21,19 +21,18 @@
 
 
 module fontROM( 
-    input wire clk, // Reloj de entrada
-    input wire [5:0] addr, // recorre todas las posibilidades de memoria 
-    output reg [7:0]  data // 8 bits
+ input wire clk, // Reloj de entrada
+ input wire [5:0] addr, // recorre todas las posibilidades de memoria 
+ output reg [7:0]  data // 8 bits
 );
 
-    reg [5:0] addr_reg; // Guarda la direccion actual
+reg [5:0] addr_reg; // Guarda la direccion actual
     
-    always @(posedge clk)
-        addr_reg <= addr; // Actualiza la direccion en cada
-                              // cambio de reloj
+always @(posedge clk)
+    addr_reg <= addr; // Actualiza la direccion en cada
+                        // cambio de reloj
     always@*
-         case (addr_reg) // Evalua el valor de la direccion
-         
+        case (addr_reg) // Evalua el valor de la direccion
          //  NULL para completar los 64bits
          6 'h00: data = 8'b00000000;
          6 'h01: data = 8'b00000000;
@@ -51,11 +50,10 @@ module fontROM(
          6 'h0d: data = 8'b00000000;
          6 'h0e: data = 8'b00000000;
          6 'h0f: data = 8'b00000000;
-         
          //  Primera letra I
          6 'h10: data = 8'b00000000;
-         6 'h11: data = 8'b11111110;
-         6 'h12: data = 8'b11111110;
+         6 'h11: data = 8'b11111111;
+         6 'h12: data = 8'b11111111;
          6 'h13: data = 8'b00111000;
          6 'h14: data = 8'b00111000;
          6 'h15: data = 8'b00111000;
@@ -63,31 +61,29 @@ module fontROM(
          6 'h17: data = 8'b00111000;
          6 'h18: data = 8'b00111000;
          6 'h19: data = 8'b00111000;
-         6 'h1a: data = 8'b11111110;
-         6 'h1b: data = 8'b11111110;
-         6 'h1c: data = 8'b00000000;
-         6 'h1d: data = 8'b00000000;
+         6 'h1a: data = 8'b00111000;
+         6 'h1b: data = 8'b00111000;
+         6 'h1c: data = 8'b11111111;
+         6 'h1d: data = 8'b11111111;
          6 'h1e: data = 8'b00000000;
          6 'h1f: data = 8'b00000000;
-         
          // Segunda letra S
          6 'h20: data = 8'b00000000;
-         6 'h21: data = 8'b01111110;
-         6 'h22: data = 8'b01111110;
-         6 'h23: data = 8'b01100000;
-         6 'h24: data = 8'b01100000;
-         6 'h25: data = 8'b01111110;
-         6 'h26: data = 8'b01111110;
-         6 'h27: data = 8'b00000110;
-         6 'h28: data = 8'b00000110;
-         6 'h29: data = 8'b00000110;
-         6 'h2a: data = 8'b01111110;
-         6 'h2b: data = 8'b01111110;
-         6 'h2c: data = 8'b00000000;
-         6 'h2d: data = 8'b00000000;
+         6 'h21: data = 8'b11111111;
+         6 'h22: data = 8'b11111111;
+         6 'h23: data = 8'b11100000;
+         6 'h24: data = 8'b11100000;
+         6 'h25: data = 8'b11100000;
+         6 'h26: data = 8'b11100000;
+         6 'h27: data = 8'b11111111;
+         6 'h28: data = 8'b11111111;
+         6 'h29: data = 8'b00000111;
+         6 'h2a: data = 8'b00000111;
+         6 'h2b: data = 8'b00000111;
+         6 'h2c: data = 8'b11111111;
+         6 'h2d: data = 8'b11111111;
          6 'h2e: data = 8'b00000000;
          6 'h2f: data = 8'b00000000;
-         
          // Tercera letra A
          6 'h30: data = 8'b00000000;
          6 'h31: data = 8'b00010000;
@@ -95,17 +91,16 @@ module fontROM(
          6 'h33: data = 8'b01101100;
          6 'h34: data = 8'b11000110;
          6 'h35: data = 8'b11000110;
-         6 'h36: data = 8'b11111110;
-         6 'h37: data = 8'b11111110;
-         6 'h38: data = 8'b11000110;
-         6 'h39: data = 8'b11000110;
+         6 'h36: data = 8'b11000110;
+         6 'h37: data = 8'b11000110;
+         6 'h38: data = 8'b11111110;
+         6 'h39: data = 8'b11111110;
          6 'h3a: data = 8'b11000110;
          6 'h3b: data = 8'b11000110;
-         6 'h3c: data = 8'b00000000;
-         6 'h3d: data = 8'b00000000;
+         6 'h3c: data = 8'b11000110;
+         6 'h3d: data = 8'b11000110;
          6 'h3e: data = 8'b00000000;
-         6 'h3f: data = 8'b00000000;
-         
+         6 'h3f: data = 8'b00000000;   
 endcase
-
+endmodule
 endmodule
